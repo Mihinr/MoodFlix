58ce4f11a8098a77ac18301556b3dfdbf1a0eb16
Mihin Ratnayake <85722458+Mihinr@users.noreply.github.com>
2024-02-08 10:10:38 +0530

    Image view model

---
+++ FinalTestNew.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+
+# Path to the saved model
+MODEL_PATH = 'working/my_cnn_model.h5'
+
+# Load the saved model
+model = tf.keras.models.load_model(MODEL_PATH)
+
+# Classes
+CLASSES = ['angry', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+
+def predict_emotion(img_path):
+    # Load and preprocess the image
+    image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    image = cv2.resize(image, (48,48), interpolation=cv2.INTER_LINEAR)
+    img = np.array(image)
+    img = img.reshape(1,48,48,1)
+    
+    # Predict the class
+    predict_x = model.predict(img) 
+    result = np.argmax(predict_x)
+    
+    # Print and display the result
+    print("Predicted class:", CLASSES[result])
+    plt.imshow(image, cmap='gray')
+    plt.show()
+
+# Example usage
+predict_emotion('input/PrivateTest_33827821.jpg')
